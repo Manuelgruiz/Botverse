06b2de3ed236ea18a75feecb690d621ba0597e5f
Mangarruiz <manuelgruiz22@gmail.com>
2024-10-22 19:58:51 +0200

    Deleting not necessary function

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -12,13 +12,3 @@ app.get("/", (req: Request, res: Response) => {
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
 });
-
-async function main() {
-  try {
-    console.log("Connection has been established successfully.");
-  } catch (err) {
-    console.error(err);
-  }
-}
-
-main();

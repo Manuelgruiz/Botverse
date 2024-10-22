82370e62fc08d3d2f128c3d6689cf22a3dbe3118
Mangarruiz <manuelgruiz22@gmail.com>
2024-10-22 19:57:35 +0200

    Change ORM sequelize---->prisma

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -12,3 +12,13 @@ app.get("/", (req: Request, res: Response) => {
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
 });
+
+async function main() {
+  try {
+    console.log("Connection has been established successfully.");
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+main();
